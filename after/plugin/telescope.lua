2603c1fbce9870834f77e42e5392269decf3f4da
jessevz <jessevanzutphen7@hotmail.com>
2024-03-30 10:13:36 +0100

    update telescope plugin

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,16 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<S-P>', builtin.git_branches, {})
+vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+vim.keymap.set('n', '<leader>pr', function ()
+    builtin.lsp_document_symbols( {ignore_symbols='variable'});
+    --builtin.lsp_document_symbols({ symbols='function' });
+end)
+
+vim.keymap.set('n', 'gr', builtin.lsp_references)
+vim.keymap.set('n', '<leader>pm', builtin.man_pages)
